f86052ad095021a464176c71964cc4510bed97ec
az1709 <davidalejo17@hotmail.com>
2025-12-11 15:35:13 -0500

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,12 +1,6 @@
-# Define Terraform provider
 terraform {
   required_version = ">= 1.3"
-  backend "azurerm" {
-    resource_group_name  = "kopicloud-tstate-rg"
-    storage_account_name = "kopicloudtfstate851"
-    container_name       = "tfstate"
-    key                  = "mLmuEG+fCj35v8CWieqbaQfsgIKLQIv/sHVVcIglDUogZpSkhC52p1RvzFtpRc5rRNlUEAJkeXCV+AStGqefbw=="
-  }
+
   required_providers {
     azurerm = {
       version = "~>3.2"
@@ -14,7 +8,7 @@ terraform {
     }
   }
 }
-# Configure the Azure provider
+
 provider "azurerm" { 
   features {}  
 }
