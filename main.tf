3a5b8d65804b76944c1c09e79861d3820da1e3f4
david17pix <dantolinez@paloaltonetworks.com>
2026-01-01 15:21:47 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,18 +1,7 @@
 resource "azurerm_resource_group" "rg" {
   name     = "yor-test-rg"
   location = "eastus"
-  tags = {
 
-    git_commit           = "1739710a5af7ff19ca14cc926572fc2986365d4c"
-    git_file             = "main.tf"
-    git_last_modified_at = "2025-12-18 23:09:46"
-    git_last_modified_by = "davidalejo17@hotmail.com"
-    git_modifiers        = "113141616+blahsi/davidalejo17"
-    git_org              = "az1709"
-    git_repo             = "DriftDetection"
-    yor_name             = "rg"
-    yor_trace            = "cc072489-4b4b-4113-a455-57bac0b35e7a"
-  }
 }
 
 resource "azurerm_storage_account" "sa" {
@@ -21,11 +10,7 @@ resource "azurerm_storage_account" "sa" {
   location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
-  tags = {
 
-    yor_name  = "sa"
-    yor_trace = "2d5e3010-9c7e-4eff-8fbf-c7889e67c0db"
-  }
 }
 
 resource "azurerm_storage_container" "container" {
